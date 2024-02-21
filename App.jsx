ce911753142a9bed6296ebe12b094c5a8bbb649c
Giovana Basílio <giovana.basilio@aluno.senai.br>
2024-02-21 09:40:39 -0300

    criacao de adicao de tarefas

--- App.jsx
+++ App.jsx
@@ -1,20 +1,53 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-      <Text>Hello, World!</Text>
+
+  const [task, setTask] = useState("");
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputLine}
+          placeholder="Digite uma nova tarefa"
+          onChangeText={setTask}
+          value={task}
+        />
+        <TouchableOpacity
+          style={styles.button}
+        >
+          <Text style={styles.inputText} >Adicionar</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
-	);
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+  },
+  inputContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  inputLine: {
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    width: "75%",
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+  },
+  inputText: {
+    color: "white",
   },
 });
